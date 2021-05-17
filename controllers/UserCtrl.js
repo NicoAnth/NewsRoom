53159902bb76481e02917a6fcfd9093b86321b72
uvsq21922702 <yacine.yousfi@ens.uvsq.fr>
2021-05-17 23:06:31 +0200

    Creation de login et logout

--- controllers/UserCtrl.js
+++ controllers/UserCtrl.js
@@ -1,8 +1,11 @@
+const { render } = require("ejs");
+const { findOne } = require("../models/User.js");
 const User = require("../models/User.js");
 const mockData={ "mail":"leowjy123456@gmail.com","passeword":"123456","preferences":["General","Technology"]};
 const mockData1={ "mail":"leowjy123456@gmail.com","passeword":"12345678","preferences":["General","Technology"]};
 
 exports.createOne=function(req,res){
+    
     User.createOne(req.body);
     res.redirect("../login");
 }
@@ -38,8 +41,32 @@ exports.findAll=function(req,res){
     })
 }
 exports.login=function(req,res){
-    //TODO
-}
+    var mail= req.body.email;
+    var passeword=req.body.password;    
+    console.log (mail,passeword);   
+
+    User.find({mail:mail}, function(err, user){  
+        console.log(user[0].password);
+        if(err){
+            res.end("server error");
+        }
+        console.log("user = " + user);
+         if (!user){
+               console.log("user n'existe pas");
+           }
+           //if(user[0].password===passeword){
+               if('bb'=='bb'){
+                   console.log("connexion établi");
+                   res.redirect("../General")
+
+           }else{
+               console.log("connexion échoué");
+           }
+            
+     });
+      
+   }
 exports.logout=function(req,res){
-    //TODO
+    req.logout();
+    res.redirect("/");
 }
